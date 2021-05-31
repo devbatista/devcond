2c14fbd5f9f30a1acc88aff3e53d19e3da2cebde
Rafael Batista <rafael@devbatista.com>
2021-05-31 11:26:09 -0300

    Criando a regra do mural de avisos

--- app/Http/Controllers/WallController.php
+++ app/Http/Controllers/WallController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Wall;
+use App\Models\User;
+use App\Models\WallLike;
 
 class WallController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $retorno = ['error' => '', 'list' => []];
+
+        $user = auth()->user();
+        
+        $walls = Wall::all();
+
+        foreach($walls as $key => $value) {
+            $walls[$key]['likes'] = 0;
+            $walls[$key]['liked'] = false;
+
+            $likes = WallLike::where('id_wall', $value['id'])->count();
+            $walls[$key]['likes'] = $likes;
+
+            $meLikes = WallLike::where('id_wall', $value['id'])->where('id_user', $user['id'])->count();
+
+            if($meLikes > 0) {
+                $walls[$key]['liked'] = true;
+            }
+        }
+        
+        $retorno['list'] = $walls;
+
+        return $retorno;
+    }
+
+    public function like($id)
+    {
+        $retorno = ['error' => ''];
+
+        $user = auth()->user();
+
+        $meLike = WallLike::where('id_wall', $id)->where('id_user', $user['id'])->count();
+
+        if($meLike) {
+            WallLike::where('id_wall', $id)->where('id_user', $user['id'])->delete();
+            $retorno['liked'] = false;
+        } else {
+            $newLike = new WallLike();
+            $newLike->id_wall = $id;
+            $newLike->id_user = $user['id'];
+            $newLike->save();
+            $retorno['liked'] = true;
+        }
+
+        $retorno['likes'] = WallLike::where('id_wall', $id)->count();
+
+        return $retorno;
+    }
 }
