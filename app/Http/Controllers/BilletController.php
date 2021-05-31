f3557325484c96910f38dcf1f811a8ab5846ce0b
Rafael Batista <rafael@devbatista.com>
2021-05-31 16:36:58 -0300

    Adicionando a funcionalidade de visualização dos boletos

--- app/Http/Controllers/BilletController.php
+++ app/Http/Controllers/BilletController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Billet;
+use App\Models\Unit;
 
 class BilletController extends Controller
 {
-    //
+    public function getAll(Request $request)
+    {
+        $retorno = ['error' => ''];
+
+        $property = $request->input('property');
+
+        if ($property) {
+            $user = auth()->user();
+            $unit = Unit::where('id', $property)->where('id_owner', $user['id'])->count();
+
+            if ($unit) {
+
+                $billets = Billet::where('id_unit', $property)->get();
+
+                foreach ($billets as $key => $value) {
+                    $billets[$key]['file_url'] = asset('storage/' . $value['file_url']);
+                }
+
+                $retorno['list'] = $billets;
+            } else {
+                $retorno['error'] = 'Esta unidade não é sua';
+            }
+        } else {
+            $retorno['error'] = 'A propriedade é obrigaória';
+        }
+
+        return $retorno;
+    }
 }
--- app/Http/Controllers/DocController.php
+++ app/Http/Controllers/DocController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Doc;
 
 class DocController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $retorno = ['error' => ''];
+
+        $docs = Doc::all();
+
+        foreach ($docs as $key => $value) {
+            $docs[$key]['file_url'] = asset('storage/'.$value['file_url']);
+        }
+
+        $retorno['list'] = $docs;
+
+        return $retorno;
+    }
 }
